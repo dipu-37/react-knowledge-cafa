9c6be04139bb377005e17b39cdc6e958668b1384
dipu-37 <razibul1105nstu@gmail.com>
2024-06-09 11:48:33 +0600

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className='text-4xl font-bold'>Knowledge Cafa</h1>
+        </div>
+    );
+};
+
+export default Header;
